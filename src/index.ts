e6b472dae229db62fbf9306a9ad280213eaa24c1
Dragon-Fish <dragon-fish@qq.com>
2023-08-28 21:54:22 +0800

    refactor: 111

--- src/index.ts
+++ src/index.ts
@@ -7,57 +7,68 @@ import { resolve } from 'path'
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url))
 const LOG_FILE = resolve(__dirname, '../output.log')
+function writeLog(content: string) {
+  writeFileSync(LOG_FILE, content + '\n', { flag: 'a' })
+}
 
 // 假装有其他东西在控制台里搞输出
 let i = 0
 const interval = setInterval(() => {
   console.log(`BOT: whatever#${i++}`)
-}, 1000)
+}, 5000)
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
+  prompt: '> ',
+})
+const getInputLine = () => `> ${rl.line}`
+
+// 正确处理退出事件，否则一辈子都退出不了
+rl.on('close', () => {
+  process.stdout.write(ansi.cursorLeft + ansi.cursorNextLine + '\n\nBye~\n\n')
+  process.exit(0)
 })
 
+// 拦截输出，确保用户输入行被置于最后一行
 intercept((text) => {
   text = text.trim()
+  writeLog('[OUTPUT_INTERCEPT]' + text)
+  const cmdStr = getInputLine()
 
-  if (text.startsWith('> ') || text.startsWith('\u001B[') || text === '\n') {
+  // 如果输出是ansi控制字符/回车键，不做特殊处理
+  if ((text.startsWith('\u001B[') || text === '\n') && text !== cmdStr) {
     return
   }
 
-  writeFileSync(LOG_FILE, text + '\n', { flag: 'a' })
+  // readline会自己打印一次按键，如果用户输入的内容与按键一致，不要重复打印
+  if (text === rl.line[rl.line.length - 1]) {
+    return ''
+  }
+
+  writeLog(text)
   // 清除用户输入行，重置光标到行首，打印最新输出，还原用户输入
-  process.stdout.write(
-    ansi.cursorLeft + ansi.eraseLine + text + '\n' + `> ${input}`
-  )
+  process.stdout.write(ansi.cursorLeft + ansi.eraseLine + text + '\n' + cmdStr)
 
   return ''
 })
 
-let input = ''
-process.stdout.write('> ')
-// process.stdin.setRawMode(true)
-// Listen for keypress event
-process.stdin.on('keypress', (str, key) => {
-  if (key.ctrl && key.name === 'c') {
-    process.stdout.write(ansi.cursorLeft + ansi.cursorNextLine + '\n\nBye~\n\n')
-    process.exit(0)
-  }
+// 当用户按下回车提交内容
+rl.on('line', (text) => {
+  process.stdout.write(ansi.cursorLeft + ansi.cursorPrevLine + ansi.eraseDown)
+  console.info('USER:', text)
+})
 
-  if (key.name === 'return') {
-    const content = input
-    input = ''
-    process.stdout.write(ansi.cursorLeft + ansi.cursorPrevLine + ansi.eraseDown)
-    console.info('USER:', content)
-  } else if (key.name === 'backspace') {
-    if (input.length === 0) return
-    input = input.slice(0, -1)
-    process.stdout.write(ansi.cursorBackward(1) + ansi.eraseEndLine)
-  } else {
-    input += str
-    process.stdout.write(
-      ansi.cursorLeft + ansi.cursorPrevLine + ansi.eraseDown + `> ${input}`
-    )
+// 检查按键按下的情况
+process.stdin.on('keypress', (str, key) => {
+  if (key.name === 'backspace') {
+    writeLog('[BACKSPACE]')
+  } else if (
+    !key.ctrl &&
+    !key.meta &&
+    key.name !== 'enter' &&
+    key.name !== 'return'
+  ) {
+    process.stdout.write(ansi.eraseEndLine + str)
   }
 })
